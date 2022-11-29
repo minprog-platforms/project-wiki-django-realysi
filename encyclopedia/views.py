e69b6de86da906d487e2a866a8d4f70512bbe58f
realysi <116087413+realysi@users.noreply.github.com>
2022-11-29 17:09:21 +0100

    wiki/entry
    kan nu wiki/etry invullen in de zoekbalk en onderaan de pagina kijken of die entry bestaat of niet

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from markdown2 import Markdown
+
+
 
 from . import util
 
@@ -8,3 +12,16 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry_page(request, title):
+    content = util.get_entry(title)
+    html_content = markdown_to_html(content) if content else None
+    return render(request, "encyclopedia/entries.html", {
+        "content": html_content,
+        "exists": content is not None,
+        "title": title if content is not None else "Error"
+        
+    })
+
+def markdown_to_html(entry):
+    to_convert = Markdown()
+    return to_convert.convert(entry)
