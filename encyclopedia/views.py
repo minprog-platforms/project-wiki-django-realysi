e4ff99d033194a7c06338a708c57a7137ae79775
realysi <116087413+realysi@users.noreply.github.com>
2022-12-06 15:51:59 +0100

    new_page update
    new_page maar zonder csrf token

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -3,10 +3,19 @@ from markdown2 import Markdown
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.http import HttpResponse
-
-
-
+from django import forms
+from django.shortcuts import redirect
+from django.core.files.storage import default_storage
+from django.contrib import messages
 from . import util
+
+
+class CreateNewPage(forms.Form):
+    title = forms.CharField(max_length=40)
+    content = forms.CharField(max_length= 2000)
+
+
+
 
 
 def index(request):
@@ -38,8 +47,21 @@ def new_page(request):
     return render(request, "encyclopedia/new_page.html")
 
 def save(request):
-    title = request.GET.get('title')
-    return HttpResponse(print(util.list_entries))
+    if request.method == "GET":
+        return render(request, "encyclopedia/new_page.html")
+    if request.method == "POST":
+        form = CreateNewPage(request.POST)
+        if not form.is_valid():
+            return render(request, "encyclopeida/new_page.html")
+        title = form.cleaned_data["title"]
+        content = form.cleaned_data["content"]
+        naam = f"entries/{title}.md"
+        if default_storage.exists(naam):
+            messages.add_message(request, messages.ERROR, 'Entry with this title already exists')
+            return render(request, "encyclopedia/new_page.html")
+        else:
+            util.save_entry(title, content)
+            return redirect("entry", title)
     #return render(request, "encyclopedia/error-message.html", {
      #       "Error-message": ""
     #    })
